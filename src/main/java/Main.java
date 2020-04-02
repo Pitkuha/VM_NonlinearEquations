8d4f9d7928ccb74bca16ba50d0a68e53c172a79b
Pitkuha <Danik2000@bk.ru>
2020-04-02 16:50:46 +0300

    v 1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static chart.Chart.show;
@@ -29,13 +31,14 @@ public class Main {
         int v = scanner.nextInt();
         double a, b, e;
         if (v == 1){
-            System.out.println(ANSI_CYAN + "Введите последовательно a, b, e" + ANSI_RESET);
+            System.out.println(ANSI_CYAN + "Введите последовательно с каждой новой строки a, b, e" + ANSI_RESET);
             a = scanner.nextDouble();
             b = scanner.nextDouble();
             e = scanner.nextDouble();
             System.out.println(otdelitel);
             HalfDivision(a,b,e,v);
             Newton(a,b,e,v);
+            SimpleIteration(a,b,e,v);
         } else if (v == 2){
             BufferedReader reader = new BufferedReader(new FileReader("./src/main/resources/iter.txt"));
             String s = reader.readLine();
@@ -45,6 +48,7 @@ public class Main {
             e = arr[2];
             HalfDivision(a,b,e,v);
             Newton(a,b,e,v);
+            SimpleIteration(a,b,e,v);
         } else System.out.println(ANSI_RED + "Ошибка выбора варианта использования" + ANSI_RESET);
 
     }
@@ -54,7 +58,6 @@ public class Main {
         if (f(a) * f(b) < 0){
         int n = 0;
         double x = 0;
-//        boolean t = abs(a - b) <= e;
         while (abs(a - b) > e) {
             x = (a + b) / 2;
             if (f(a) * f(x) > 0) {
@@ -132,11 +135,68 @@ public class Main {
     }
 
     public static void SimpleIteration(double a, double b, double e, int output){
-        double lambda = - 1/max(df1(a),df1(b));
-        double x = a;
-        while ()
+        if (f(a) * f(b) < 0) {
+            double[] arr;
+            List<Double> list = new ArrayList<>();
+            list.add((a + b) / 2);
+            int i = 0;
+            if (abs(dfif1(a)) < 1 && abs(dfif1(b)) < 1) {
+                while (true) {
+                    list.add(fif1(list.get(i)));
+                    i++;
+                    if (abs(list.get(i) - list.get(i - 1)) <= e) {
+                        break;
+                    }
+                }
+            } else if (abs(dfif2(a)) < 1 && abs(dfif2(b)) < 1) {
+                while (true) {
+                    list.add(fif2(list.get(i)));
+                    i++;
+                    if (abs(list.get(i) - list.get(i - 1)) <= e) {
+                        break;
+                    }
+                }
+            } else if (abs(dfif3(a)) < 1 && abs(dfif3(b)) < 1) {
+                while (true) {
+                    list.add(fif3(list.get(i)));
+                    i++;
+                    if (abs(list.get(i) - list.get(i - 1)) <= e) {
+                        break;
+                    }
+                }
+            } else {
+                while (true) {
+                    list.add(fifa(list.get(i), a, b));
+                    i++;
+                    if (abs(list.get(i) - list.get(i - 1)) <= e) {
+                        break;
+                    }
+                }
+            }
+            if (output == 1) {
+                System.out.println("Метод простой итерации");
+                System.out.println("Кол-во итераций = " + i);
+                System.out.println("Найденный корень уравнения x = " + list.get(i));
+                System.out.println("Значение функции в корне f(x) = " + f(list.get(i)));
+                System.out.println(otdelitel);
+            } else if (output == 2){
+                try(FileWriter writer = new FileWriter("./src/main/resources/result.txt", true)){
+                    writer.write("=============================================================================");
+                    writer.append('\n');
+                    writer.write("Метод Простой итерации");
+                    writer.append('\n');
+                    writer.write("Кол-во итераций = " + i);
+                    writer.append('\n');
+                    writer.write("Найденный корень уравнения x = " + list.get(i));
+                    writer.append('\n');
+                    writer.write("Значение функции в корне f(x) = " + f(list.get(i)));
+                    writer.append('\n');
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        } else System.out.println("На данном пром-ке нет корней");
     }
-
 
     //функции
     public static double f(double x){
@@ -157,24 +217,22 @@ public class Main {
     }
 
     public static double fif2(double x){
-        return sqrt((pow(x,3) - 17.37 * x + 5.38)/-4.81);
+        return sqrt((pow(x,3) - 17.37 * x + 5.38)/(-4.81));
     }
 
     public static double fif3(double x){
         return pow(-4.81 * pow(x,2) + 17.37 * x - 5.38, 1/3);
     }
 
-    //функции для примера
-    public static double t(double t){
-        return pow(t, 3) - t + 4;
+    //x + l * f(x)
+    public static double fifa(double x, double a, double b){
+        double l = -1 / max(df1(a), df1(b));
+        return x + l * f(x);
     }
 
-    public static double dt1(double t){
-        return 3 * pow(t, 2) - 1;
-    }
+    public static double dfif1(double x) { return 0.1727 * pow(x,2) + 0.5538 * x; }
 
-    public static double dt2(double t){
-        return 6 * t;
-    }
+    public static double dfif2(double x) { return (-0.3119 * pow(x,2) + 1.8056)/sqrt((pow(x,3) - 17.37 * x + 5.38)/(-4.81)); }
 
+    public static double dfif3(double x) { return (-3.2067 * x + 5.79)/pow((-4.81 * pow(x,2) + 17.37 * x - 5.38),2/3); }
 }
